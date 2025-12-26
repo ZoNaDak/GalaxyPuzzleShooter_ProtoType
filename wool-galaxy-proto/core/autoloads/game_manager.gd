0fa72b6ddcdf46f50cd440179b7a8d0984957b6d
ZoNaDak <kbh3719net@gmail.com>
2025-12-27 00:58:27 +0900

    game_manager 와 연동

--- wool-galaxy-proto/core/autoloads/game_manager.gd
+++ wool-galaxy-proto/core/autoloads/game_manager.gd
@@ -2,26 +2,41 @@
 extends Node
 
 #region Enums
+
 enum GameState {
 	NONE,
 	TITLE,
 	MAIN,
 }
+
 #endregion
 
 #region Signals
+
 signal state_changed(new_state: GameState)
+
 #endregion
 
 #region Variables
+
 var current_state: GameState = GameState.NONE
+
 #endregion
 
 #region Methods
+
+func init_state(new_state: GameState) -> void:
+	print("[GameManager] init_state: ", GameState.keys()[new_state])
+	current_state = new_state
+
 func change_state(new_state: GameState) -> void:
 	if current_state == new_state:
 		return
+	var old_state := current_state
 	current_state = new_state
+	print("[GameManager] change_state: ", GameState.keys()[old_state], " -> ", GameState.keys()[new_state])
 	state_changed.emit(new_state)
+	# TODO: 씬 변경 로직 추가
+
 #endregion
 
--- wool-galaxy-proto/game/scenes/title_scene.gd
+++ wool-galaxy-proto/game/scenes/title_scene.gd
@@ -2,13 +2,20 @@
 extends Node
 
 #region Signals
+
 #endregion
 
 #region Variables
+
 #endregion
 
 #region Lifecycle
+
+func _ready() -> void:
+	GameManager.init_state(GameManager.GameState.TITLE)
+
 #endregion
 
 #region Methods
+
 #endregion
