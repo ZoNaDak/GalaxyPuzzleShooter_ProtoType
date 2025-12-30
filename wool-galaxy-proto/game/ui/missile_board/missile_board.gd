0b024520e0c7340409efefca57ab9cab911ba0ca
ZoNaDak <kbh3719net@gmail.com>
2025-12-31 01:50:44 +0900

    Refactor Code

--- wool-galaxy-proto/game/ui/missile_board/missile_board.gd
+++ wool-galaxy-proto/game/ui/missile_board/missile_board.gd
@@ -1,12 +1,28 @@
 @tool
 extends Control
 
+#region Consts
+
 const BASE_SIZE := Vector2(576, 384)
+
+#endregion
+
+#region Variables
+
+@export var _back_board: TextureRect
+
+#endregion
+
+#region Lifecycle
 
 func _ready():
 	if not Engine.is_editor_hint():
 		resized.connect(_on_resized)
 	_update_layout()
+
+#endregion
+
+#region Signal Handlers
 
 func _notification(what):
 	if what == NOTIFICATION_RESIZED:
@@ -15,23 +31,23 @@ func _notification(what):
 func _on_resized():
 	_update_layout()
 
+#endregion
+
+#region Methods
+
 func _update_layout():
-	if get_child_count() == 0:
-		return
-	
-	var child = get_child(0)
-	if not child:
-		return
-	
+
 	# 크기가 0이면 아직 레이아웃이 안된 것
 	if size.x <= 0 or size.y <= 0:
 		return
 	
 	# 스케일 계산
 	var scale_factor = min(size.x / BASE_SIZE.x, size.y / BASE_SIZE.y)
-	child.scale = Vector2(scale_factor, scale_factor)
+	_back_board.scale = Vector2(scale_factor, scale_factor)
 	
 	# 하단 중앙 정렬
 	var scaled_size = BASE_SIZE * scale_factor
-	child.position.x = (size.x - scaled_size.x) / 2
-	child.position.y = size.y - scaled_size.y
+	_back_board.position.x = (size.x - scaled_size.x) / 2
+	_back_board.position.y = size.y - scaled_size.y
+
+#endregion
