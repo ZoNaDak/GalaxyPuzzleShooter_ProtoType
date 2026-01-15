691f5ba6c6af27ed2dbbf6212297813a85905d69
ZoNaDak <kbh3719net@gmail.com>
2026-01-16 00:50:22 +0900

    Apply Missile Slot Sprite

--- wool-galaxy-proto/game/ui/missile_slot/missile_slot_board.gd
+++ wool-galaxy-proto/game/ui/missile_slot/missile_slot_board.gd
@@ -12,9 +12,10 @@ class_name MissileSlotBoard
 #region Lifecycle
 
 func _ready():
-	missile_slots[0].setup_missile_data(Enums.MissileColorType.RED, 1)
-	missile_slots[1].setup_missile_data(Enums.MissileColorType.BLUE, 2)
-	missile_slots[2].setup_missile_data(Enums.MissileColorType.GREEN, 3)
+    if DebugUtils.try_center_if_root(self):
+        missile_slots[0].setup_missile_data(Enums.MissileColorType.RED, 1)
+        missile_slots[1].setup_missile_data(Enums.MissileColorType.BLUE, 2)
+        missile_slots[2].setup_missile_data(Enums.MissileColorType.GREEN, 3)   
 
 #endregion
 
