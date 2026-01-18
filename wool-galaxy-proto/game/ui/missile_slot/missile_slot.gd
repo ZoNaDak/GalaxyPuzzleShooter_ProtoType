89a302474ada1ecf21a1e3d77a64238c2af55bc3
ZoNaDak <kbh3719net@gmail.com>
2026-01-18 17:21:16 +0900

    Refactor MissileData

--- wool-galaxy-proto/game/ui/missile_slot/missile_slot.gd
+++ wool-galaxy-proto/game/ui/missile_slot/missile_slot.gd
@@ -8,6 +8,8 @@ class_name MissileSlot
 @export var _missile_texture: TextureRect
 @export var _missile_amount: Label
 
+var missile_data: MissileData = null
+
 #endregion
 
 #region Lifecycle
@@ -24,10 +26,10 @@ func setup_empty():
     _missile_texture.texture = null
     _missile_amount.visible = false
 
-func setup_missile_data(color: Enums.MissileColorType, amount: int):
+func setup_missile_data(data: MissileData):
     _missile_amount.visible = true
-    var sprite_path := MissileData.get_sprite_path(color, Enums.MissileSizeType.MEDIUM)
+    var sprite_path := MissileData.get_sprite_path(data.color, Enums.MissileSizeType.MEDIUM)
     _missile_texture.texture = load(sprite_path) as Texture2D
-    _missile_amount.text = str(amount)
+    _missile_amount.text = str(data.amount)
 
 #endregion
--- wool-galaxy-proto/game/ui/missile_slot/missile_slot_board.gd
+++ wool-galaxy-proto/game/ui/missile_slot/missile_slot_board.gd
@@ -12,10 +12,9 @@ class_name MissileSlotBoard
 #region Lifecycle
 
 func _ready():
-    if DebugUtils.try_center_if_root(self):
-        missile_slots[0].setup_missile_data(Enums.MissileColorType.RED, 1)
-        missile_slots[1].setup_missile_data(Enums.MissileColorType.BLUE, 2)
-        missile_slots[2].setup_missile_data(Enums.MissileColorType.GREEN, 3)   
+	_missile_slots[0].setup_empty()
+	_missile_slots[1].setup_empty()
+	_missile_slots[2].setup_empty()
 
 #endregion
 
